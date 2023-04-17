b093bd46cf88e4ed3f6455e97bae5749946d6e09
XChasandriX <101826720+XChasandriX@users.noreply.github.com>
2023-04-17 18:18:04 +0000

    Add files via upload

---
+++ tetris.js
@@ -0,0 +1,127 @@
+'use strict'
+//CONSTANTES
+const W = 10//Ancho del tetris
+const H = 10//Altura del tetris
+const L = 30
+const forma = []
+const colores = ["red", "green", "cyan", "pink", "purple", "orange"]
+//cuadrado
+forma[0] = [[1,1],[1,1]]
+//palo
+forma[1] = [[1,1,1,1]]
+//T
+forma[2] = [[1,1,1],[0,1,0]]
+//L
+forma[3] = [[1,1,1],[0,0,1]]
+//J
+forma[4] = [[1,1,1],[1,0,0]]
+//S
+forma[5] = [[1,1,0],[0,1,1]]
+//Z
+forma[6] = [[0,1,1],[1,1,0]]
+var canvas, ctx;
+let pieza
+let mapa = []
+for (let i = 0; i < H; i++){
+  mapa[i] = []
+  for (let j = 0; j < W; j++)
+    mapa[i][j]= 0
+}
+//CÓDIGO
+window.onload = iniciar
+window.onkeydown = presionar
+//FUNCIONES
+function presionar(event){//Mover con flechas
+  if (event.key == "ArrowLeft")
+    pieza.col--
+  if (event.key == "ArrowRight")
+    pieza.col++
+  if (event.key == "ArrowDown")
+    pieza.fila++
+  if (pieza.col < 0)
+    pieza.col = 0
+  if (pieza.col > (W - pieza.forma[0].length) ){
+    pieza.col = W - pieza.forma[0].length
+  }
+}
+function crearPieza(){
+  let nume = Math.floor(Math.random()*forma.length)
+  pieza ={
+  forma: forma[nume],
+  fila: -1,
+  col: W/2,
+  color: colores[nume]
+  }
+}
+function generar(){
+  crearPieza()
+
+}
+function iniciar(){
+  canvas = document.getElementById("canvas");//canvas
+  ctx = canvas.getContext("2d");
+  canvas.width = L * W
+	canvas.height = L * H
+	ctx.lineWidth = 2
+	ctx.strokeStyle = 'white'
+  generar()
+  setInterval(animadors, 1000)
+}
+function dibujarcubo(fila, col, color){
+  ctx.beginPath()
+  ctx.moveTo(col*L, fila*L)
+  ctx.lineTo((col+1)*L, fila*L)
+  ctx.lineTo((col+1)*L, (fila+1)*L)
+  ctx.lineTo(col*L, (fila+1)*L)
+  ctx.lineTo(col*L, fila*L)
+  ctx.fillStyle = color
+  ctx.stroke()
+  ctx.fill()
+}
+function dibujarpieza(piz){//pintar pieza
+  for(let fila = 0; fila < piz.forma.length; fila++)
+    for(let col = 0; col < piz.forma[fila].length; col++){
+      if (piz.forma[fila][col] == 1)
+        dibujarcubo(piz.fila+fila, piz.col+col, piz.color)
+    }
+}
+function animadors(){
+  ctx.clearRect(0 ,0 , W*L, H*L);
+  //Si choca, la añadimos a piezas
+  if(hachocado(pieza)){
+    console.log('choca')
+    generar()
+  }
+  //La movemos hacia abajo
+  else{
+    pieza.fila = pieza.fila+1
+    if (pieza.fila >  H-3){
+        metermapa(pieza)
+        generar()
+        console.log(mapa)
+      }
+  }
+    dibujarpieza(pieza)
+    dibujarmapa()
+}
+function metermapa(pieza){
+  for(let f = 0; f < pieza.forma.length; f++)
+    for(let c = 0; c < pieza.forma[f].length; c++){
+      console.log(pieza.forma[f][c] == 1)
+      if(pieza.forma[f][c] == 1)
+        mapa[f+pieza.fila][c+pieza.col] = pieza.color
+    }
+}
+function hachocado(pieza){
+  //for (let fila = 0; fila < pieza.forma.length; fila++)
+  //Si llega al suelo, la añadimos a piezas
+  return false
+}
+function dibujarmapa(){
+  for(let f = 0; f < mapa.length; f++)
+    for(let c = 0; c < mapa[f].length; c++){
+      if(mapa[f][c] == 1)
+        dibujarcubo(f, c, mapa[f][c])
+  }
+}
+//////////////////////////////////////////////////////////////////
